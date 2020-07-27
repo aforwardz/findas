cce93cf4a9238deb9b37261dc2c75a1b60350d01
aforwardz <aforwardzyu@gmail.com>
2020-07-27 17:57:27 +0800

    update stratege base

--- findas/strategy/StrategyBase.py
+++ findas/strategy/StrategyBase.py
@@ -14,30 +14,37 @@ class StrategyBase(object):
     def __init__(self):
         pass
 
-    def buy_
-
     def simulate_transaction(self):
         if self.df.empty:
             return
         share = INIT_SHARE
 
-        empty, last_market, market_list = True, None, []
+        last_state, last_market, market_list = 0, None, []
         for i, r in self.df.iterrows():
-            if empty and r.deal == -1:
-                market_list.append({'date': i, 'market': last_market})
-                continue
-            if r.deal == -1:
-                share = last_market / r.price
-                last_market = None
-                empty = True
-            elif r.deal == 0:
-                if not empty:
+            if r.deal == 1:
+                if r.deal == last_state:
+                    # 继续持有
                     last_market = r.price * share
                 else:
-
+                    # 重新买入
+                    if last_state == 0:
+                        last_market = r.price * share
+                    else:
+                        share = last_market / r.price
             else:
-                last_market = r.price * share
-                empty = False
+                if r.deal == last_state:
+                    # 继续空仓
+                    pass
+                else:
+                    # 清仓
+                    last_market = r.price * share
+                    share = 0
 
             market_list.append({'date': i, 'market': last_market})
+
+            last_state = r.deal
+
+        market_df = pd.DataFrame(data=market_list)
+        market_df = market_df.set_index('date')
+        self.df = self.df.join(market_df)
 
